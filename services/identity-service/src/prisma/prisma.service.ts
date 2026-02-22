08b45cc7a56d50bf236aa0bd16ec19d986a566fc
Bimsara Janakantha <janakanthaofficial@gmail.com>
2026-02-22 23:02:21 +0530

    Prisma configured

---
+++ services/identity-service/src/prisma/prisma.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import type { OnModuleInit, OnModuleDestroy } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+    console.log("Prisma connected to the database");
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log("Prisma disconnected from the database");
+  }
+}
