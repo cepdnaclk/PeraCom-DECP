9c21a3f377987fe8c1c7637e5864aff538eb11b7
Bimsara Janakantha <janakanthaofficial@gmail.com>
2026-02-25 15:05:14 +0530

    Update update-profile.dto.ts

--- services/identity-service/src/users/dto/update-profile.dto.ts
+++ services/identity-service/src/users/dto/update-profile.dto.ts
@@ -1,4 +1,11 @@
-import { IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator";
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  IsUUID,
+  MaxLength,
+} from "class-validator";
 
 export class UpdateProfileDto {
   @IsNotEmpty()
@@ -7,33 +14,40 @@ export class UpdateProfileDto {
 
   @IsOptional()
   @IsString()
+  @MaxLength(50)
   first_name?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(100)
   middle_name?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(50)
   last_name?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(255)
   residence?: string;
 
   @IsOptional()
-  @IsString()
+  @IsUrl()
   profile_pic?: string;
 
   @IsOptional()
   @IsString()
+  @IsUrl()
   header_img?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(255)
   headline?: string;
 
   @IsOptional()
   @IsString()
+  @MaxLength(1000)
   bio?: string;
 }
