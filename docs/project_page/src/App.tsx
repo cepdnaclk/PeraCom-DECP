2f0da1dd8acee004e339d883b60c4426be7864bc
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2026-03-16 00:22:22 +0530

    Update App.tsx

--- docs/project_page/src/App.tsx
+++ docs/project_page/src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/PeraCom-DECP">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
