ca343cddf57288994f1a5254055fb7ddbbf483cb
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2026-04-24 23:58:44 +0530

    Update vite.config.ts

--- docs/project_page/vite.config.ts
+++ docs/project_page/vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/e20-co528-PeraCom-DECP/",
+  base: "/PeraCom-DECP/",
   build: {
     outDir: "dist",
   },
